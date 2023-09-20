56cd4fca397425ac9a4c3988a35dd82272f6c967
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-09-20 10:16:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inlämningsuppgift_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill di hyra en Firari");
+            float X = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många kilometer ska du köra?");
+            float Y = float.Parse(Console.ReadLine());
+            float Z = 300 + 1 * Y + 500 * X - 500;
+            Console.WriteLine("Totalt pris " + Z + " kronor ");
+        }
+    }
+}
